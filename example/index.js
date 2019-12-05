6a936a8707900fb1741633e1b1dc1b29a086047b
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-12-05 22:20:23 +0100

    Add asyncHandler/Middleware example :framed_picture:

---
+++ example/index.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+const delay = require('delay');
+const express = require('express');
+const {asyncHandler, asyncMiddleware} = require('../src'); // use 'async-express-helpers' in practice
+
+const app = express();
+
+app.use(
+  asyncMiddleware(async (req, res, next) => {
+    if (req.query.error === 'explicit') throw new Error('issue in explicit next async middleware');
+    req.explicit = true;
+    await delay(22);
+    return next();
+  })
+);
+
+app.use(
+  asyncMiddleware(async req => {
+    if (req.query.error === 'implicit') throw new Error('issue in implicit next async middleware');
+    req.implicit = true;
+    await delay(12);
+  }, true)
+);
+
+app.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    if (req.query.error === 'handler') throw new Error('issue in async handler');
+    await delay(22);
+    res.send(`Hello I'm async`);
+  })
+);
+
+app.use((err, req, res, next) => {
+  res.status(500).send(`ERROR OCCURED: ${err.message}`);
+});
+
+app.listen(3223, () => console.log('hit me on localhost:3223'));
