66ef06872ff9073485ff11f0c3f02ee6c155d1f8
AdrieanKhisbe <adriean.khisbe@live.fr>
2020-02-20 03:14:37 +0100

    Add some basic jsdoc in the source :open_book:

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+/**
+ * Generate an express "handler" out of provided async middleware-like function
+ *
+ * @param {*} handler - async function to be wrapped (and injected req(uest) and res(ponse) objects)
+ */
 const asyncHandler = handler => async (req, res, next) => {
   try {
     await handler(req, res, next);
@@ -6,6 +11,12 @@ const asyncHandler = handler => async (req, res, next) => {
   }
 };
 
+/**
+ * Generate an express middleware out of provided async middleware-like function
+ *
+ * @param {*} middleware - async function to be wrapped
+ * @param {Boolean} implicitNext - tells whether next middleware should be implicitely called [default false]
+ */
 const asyncMiddleware = (middleware, implicitNext = false) => async (req, res, next) => {
   try {
     const shouldCallNext = await middleware(req, res, next);
